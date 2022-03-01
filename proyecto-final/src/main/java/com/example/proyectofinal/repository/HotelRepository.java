d0697605caea35062276022833d624327d488b8f
raulXprtl <raul.ceron@xpertal.com>
2022-03-01 11:14:07 -0600

    Agrega repositorios de entidades

--- proyecto-final/src/main/java/com/example/proyectofinal/repository/HotelRepository.java
+++ proyecto-final/src/main/java/com/example/proyectofinal/repository/HotelRepository.java
@@ -2,6 +2,8 @@ package com.example.proyectofinal.repository;
 
 import com.example.proyectofinal.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, String> {
 }
--- proyecto-final/src/main/java/com/example/proyectofinal/repository/ReservationRepository.java
+++ proyecto-final/src/main/java/com/example/proyectofinal/repository/ReservationRepository.java
@@ -1,9 +1,9 @@
 package com.example.proyectofinal.repository;
 
-import com.example.proyectofinal.entity.Flight;
+import com.example.proyectofinal.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlightRepository extends JpaRepository<Flight, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 }
